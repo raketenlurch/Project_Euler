01a815779ff7c77d6729d4c487ed6e1d8655d038
raketenlurch <annkatrin.le@gmx.de>
2020-01-17 21:00:14 +0100

    Add Multiples.py in Python

---
+++ Python/Problem01/Multiples.py
@@ -0,0 +1,14 @@
+# Source: https://projecteuler.net/problem=1
+#  If we list all the natrual numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9.
+#  The sum of these multiples es 23.
+#
+#  Find the sum of all the multiples of 3 or 5 below 1000.
+
+number = 1000
+sum = 0
+
+for i in range(0, number):
+    if i%3 == 0 or i%5 == 0:
+        sum += i
+
+print(sum)
