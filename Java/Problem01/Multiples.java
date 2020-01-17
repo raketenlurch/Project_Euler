b949f6566aaec21b0fad6377b51a128ff6e529df
raketenlurch <annkatrin.le@gmx.de>
2020-01-17 15:46:16 +0100

    Initial commit

---
+++ Java/Problem01/Multiples.java
@@ -0,0 +1,17 @@
+/* Source: https://projecteuler.net/problem=1
+*  If we list all the natrual numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6, and 9.
+*  The sum of these multiples es 23.
+*
+*  Find the sum of all the multiples of 3 or 5 below 1000.
+*/
+
+public class Multiples {
+  public static void main(String[] args) {
+    int number = 1000;
+    int sum    = 0;
+
+    for () {
+
+    }
+  }
+}
