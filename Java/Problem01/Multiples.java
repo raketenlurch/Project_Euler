e2bc13e024621364b783964f2bbd6a35f47da06a
raketenlurch <annkatrin.le@gmx.de>
2020-01-17 19:01:10 +0100

    Add Problem 01

--- Java/Problem01/Multiples.java
+++ Java/Problem01/Multiples.java
@@ -10,8 +10,11 @@ public class Multiples {
     int number = 1000;
     int sum    = 0;
 
-    for () {
-
+    for (int i=0; i<number; i++) {
+      if (i%3 == 0 || i%5 == 0) {
+        sum += i;
+      }
     }
+    System.out.println(sum);
   }
 }
